820144f9b256f3b4fdbacc3ae8e693cda3cc721c
jjeevanjyoti <jjeevanjyoti@gmail.com>
2019-11-14 16:25:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'filereader';
+  csvContent: string;
+
+  constructor(){}
+  ngOnInit(){
+  }
+  onFileLoad(fileLoadedEvent) {
+    const textFromFileLoaded = fileLoadedEvent.target.result;              
+    this.csvContent = textFromFileLoaded;   
+    console.log(this.csvContent)  
+     console.log(typeof(this.csvContent));
+ 
+  
+}
+
+onFileSelect(input: HTMLInputElement) {
+
+const files = input.files;
+var content = this.csvContent;    
+if (files && files.length) {
+  console.log(files)
+  const fileToRead = files[0];
+
+  const fileReader = new FileReader();
+  fileReader.onload = this.onFileLoad;
+
+ fileReader.readAsText(fileToRead, "UTF-8");
+ 
+ }
+
+ }
 }
